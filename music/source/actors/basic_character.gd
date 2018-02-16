a5af428267657a240e9dc74fc6eb3e2bf61b154f
Henrique Campos <henriiquecampos@gmail.com>
2018-02-16 09:36:03 -0200

    Added core features (#2)
    * Added main character
    
    Also added gitignore for promo stuff
    
    * Added tileset
    
    * Added Tileset resource to project
    
    * Added test level and initialized note class
    
    * Added Note duration mechanic
    
    * Added Note Pitch
    
    Also fixed loop so that the note can be hold
    
    * Removed unused files
    
    * Added birbs sprites
    
    * Changed birbs sprites
    
    * Removed unused shapes

---
+++ music/source/actors/basic_character.gd
@@ -0,0 +1,63 @@
+extends KinematicBody2D
+
+const UP = Vector2(0, -1)
+const GRAVITY = 20
+const MAX_SPEED = 300
+const JUMP_HEIGHT = -600
+var velocity = Vector2()
+var can_jump = true
+
+enum STATES{IDLE, WALK, JUMP}
+var current_state = IDLE setget set_current_state, get_current_state
+
+func _physics_process(delta):
+	velocity.y += GRAVITY
+	velocity = move_and_slide(velocity, UP)
+	
+	if is_on_floor():
+		can_jump = true
+		
+	if !can_jump:
+		if velocity.y == 0:
+#			$Sprite.set_animation("fall")
+			$Sprite.stop()
+		elif velocity.y > 10:
+#			$Sprite.set_frame(1)
+			pass
+	
+func apply_jump():
+	if is_on_floor():
+		set_current_state(JUMP)
+		velocity.y = JUMP_HEIGHT
+		can_jump = false
+		
+func cancel_jump():
+	if !can_jump:
+		if velocity.y <= 0:
+			velocity.y = 0
+		
+func walk(direction, speed = MAX_SPEED):
+	velocity.x = speed * direction
+	
+	if direction != 0 and can_jump:
+		set_current_state(WALK)
+		$Sprite.set_flip_h(false if direction > 0 else true)
+		$Sprite.set_offset(Vector2(-12 * direction, -64))
+	else:
+		if can_jump:
+			set_current_state(IDLE)
+
+func set_current_state(state):
+	if current_state == state:
+		return
+#	if state == IDLE:
+#		$Sprite.play("idle")
+#	elif state == WALK:
+#		$Sprite.play("walk")
+#	elif state == JUMP:
+#		$Sprite.play("jump")
+		
+	current_state = state
+
+func get_current_state():
+	return(current_state)
---
+++ music/source/actors/player/player_character.gd
@@ -0,0 +1,14 @@
+extends "res://actors/basic_character.gd"
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_up"):
+		apply_jump()
+	if Input.is_action_just_released("ui_up"):
+		cancel_jump()
+		
+	if Input.is_action_pressed("ui_right"):
+		walk(1)
+	elif Input.is_action_pressed("ui_left"):
+		walk(-1)
+	else:
+		walk(0)
