bc228d1f1a7ffa8b04062d3c257e7f7012fca695
Henrique Campos <henriiquecampos@gmail.com>
2018-03-12 13:04:36 -0300

    Added Fighting character

--- permanence/source/screens/basic_screen.gd
+++ permanence/source/screens/basic_screen.gd
@@ -4,18 +4,14 @@ export (String, FILE, "*.tscn") var next_scene
 signal transition_finished
 
 func _ready():
-	#Transition back everytime a screen is loaded
 	apply_transition(OUT)
 	
 func change_scene(to = next_scene):
-	#Applies an transition animation then load the next scene
 	apply_transition(IN)
 	yield(self, "transition_finished")
 	get_tree().change_scene(to)
 	
 func apply_transition(mode):
-	#Verifies which kind of transition to be used, IN or OUT then plays
-	#the animation forward or backwards then emits a signal for completion
 	var a = $Animator
 	if mode == 0:
 		a.play("transition")
