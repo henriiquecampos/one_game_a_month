061e8ef67fde20610f179a483aa8bb6df8d5a3c4
Henrique Campos <henriiquecampos@gmail.com>
2018-03-12 10:28:54 -0300

    Added inputs handling for player character

---
+++ permanence/source/actors/player/player_character.gd
@@ -0,0 +1,36 @@
+extends "res://actors/basic_kinematic_character.gd"
+
+
+func _input(event):
+	if not event is InputEventKey:
+		return
+	
+	if event.is_action_pressed("jump"):
+		set_state(JUMP)
+	elif event.is_action_released("jump"):
+		match state:
+			JUMP:
+				cancel_jump()
+				
+	if event.is_action_pressed("right"):
+		direction = 1
+		match state:
+			IDLE:
+				set_state(WALK)
+			JUMP:
+				velocity.x = direction * in_jump_speed
+	elif event.is_action_released("right"):
+		match state:
+			WALK:
+				set_state(IDLE)
+	if event.is_action_pressed("left"):
+		direction = -1
+		match state:
+			IDLE:
+				set_state(WALK)
+			JUMP:
+				velocity.x = direction * in_jump_speed
+	elif event.is_action_released("left"):
+		match state:
+			WALK:
+				set_state(IDLE)
