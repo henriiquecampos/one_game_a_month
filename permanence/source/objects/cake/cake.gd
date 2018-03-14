04d2c24e7fd3555112c43196635f90bf1e4d2e48
Henrique Campos <henriiquecampos@gmail.com>
2018-03-14 10:48:46 -0300

    Changed cake get_hurt behavior

--- permanence/source/objects/cake/cake.gd
+++ permanence/source/objects/cake/cake.gd
@@ -3,14 +3,19 @@ extends "res://objects/basic_candy.gd"
 export (int) var SPEED = 100
 const SPRITES = [preload("res://objects/cake/choco_cake.png"), preload("res://objects/cake/strawberry_cake.png")]
 
+var velocity = Vector2(1,0)
 func _ready():
+	if get_position().x > get_viewport().size.x/2:
+		velocity.x *= -1
 	randomize()
 	$sprite.set_texture(SPRITES[randi()%SPRITES.size()])
+
 func _physics_process(delta):
-	move_local_x(SPEED * delta)
+	move_local_x((velocity.x * SPEED) * delta)
+	move_local_y((velocity.y * SPEED) * delta)
 	
-func get_hit():
+func get_hit(hit_normal):
 	.get_hit()
-	SPEED = (SPEED * 0.75) * -1
+	velocity = hit_normal
 	yield($visibility, "screen_exited")
 	set_state(DEAD)
