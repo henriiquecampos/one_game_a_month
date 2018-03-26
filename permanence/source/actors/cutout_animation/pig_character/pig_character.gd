61b650544ce43dcd0f536e86dd5bba7923fa4a63
Henrique Campos <henriiquecampos@gmail.com>
2018-03-26 09:40:24 -0300

    Added visual feedback for pig's fatness

--- permanence/source/actors/cutout_animation/pig_character/pig_character.gd
+++ permanence/source/actors/cutout_animation/pig_character/pig_character.gd
@@ -3,8 +3,8 @@ extends Node2D
 onready var initial_scale = get_scale()
 
 func _input(event):
-		
-	if event.is_action_pressed("right"):
+
+	if event.is_action_pressed("right") or Input.get_joy_axis(0, 0) == 1:
 		set_scale(Vector2(initial_scale.x, initial_scale.y))
-	elif event.is_action_pressed("left"):
+	elif event.is_action_pressed("left") or Input.get_joy_axis(0, 0) == -1:
 		set_scale(Vector2(initial_scale.x * -1, initial_scale.y))
